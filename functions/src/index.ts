12cb8cf3b96db628dc0483ee9949d1d1416433d2
Dalia <daliaabd123q@gmail.com>
2025-01-27 07:14:23 -0800

    Your commit message describing the new version

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,21 +1,49 @@
-/**
- * Import function triggers from their respective submodules:
- *
- * import {onCall} from "firebase-functions/v2/https";
- * import {onDocumentWritten} from "firebase-functions/v2/firestore";
- *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
- */
+import { onRequest } from "firebase-functions/v2/https";
+import { logger } from "firebase-functions";
+import { defineString } from "firebase-functions/params";
 
-import {onRequest} from "firebase-functions/v2/https";
-import * as logger from "firebase-functions/logger";
+// Define an environment variable
+const GREETING = defineString('GREETING', { default: 'Hello from Firebase!' });
 
-// Start writing functions
-// https://firebase.google.com/docs/functions/typescript
+export const helloWorld = onRequest(async (request, response) => {
+  try {
+    // Log the function execution
+    logger.info("Function executed", {
+      functionName: "helloWorld",
+      requestMethod: request.method,
+      requestPath: request.path,
+      requestQuery: request.query,
+      requestHeaders: request.headers,
+    });
 
-export const helloWorld = onRequest((request, response) => {
-  logger.info("Hello logs!", {structuredData: true});
-  response.send("Hello from Firebase!");
+    // Get the name from the query parameter, or use a default
+    const name = request.query.name || 'World';
+
+    // Use the environment variable
+    const greeting = GREETING.value();
+
+    // Prepare the response
+    const responseMessage = `${greeting} ${name}!`;
+
+    // Set response headers
+    response.set('Content-Type', 'application/json');
+
+    // Send the response
+    response.status(200).json({
+      message: responseMessage,
+      timestamp: new Date().toISOString()
+    });
+
+  } catch (error) {
+    // Log the error
+    logger.error("Error in helloWorld function", error);
+
+    // Send an error response
+    response.status(500).json({
+      error: "An internal error occurred",
+      timestamp: new Date().toISOString()
+    });
+  }
 });
 
 // If you're not using any other functions, you can keep them commented out
